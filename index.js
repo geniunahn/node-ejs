de88fe1a535acf0ad105b90884b1610f3ef24a4a
geniunahn <geniunahn@naver.com>
2022-12-19 16:26:20 +0900

    form의 input 정보가 입력되면 새로고침되면서 페이지에 랜더함

--- index.js
+++ index.js
@@ -7,29 +7,28 @@ app.set("view engine", "ejs");
 // 정적파일 경로 지정
 app.use(express.static("public"));
 
-// home 라우팅
+// home
 app.get("/", (req, res) => {
-  const title = "홈페이지";
-  const name = "홍길동";
-  //   name 변수 값을 index.ejs 페이지에 전달한다는 의미다.
   res.render("pages/index.ejs", {
-    // 아래는 위의 변수를 그대로 가져와서 페이지에 전달하는 방식이다. 반드시 위에서 변수를 만들어야 한다.
-    title,
-    name,
-    // 아래의 왼쪽 이름은 객체의 이름이고 오른쪽 이름은 객체의 값이다. 자료형 객체의 형태로 변수를 index.ejs 문서에게 보내는 방법이다. 따로 const, let 으로 변수를 만들 필요가 없다.
-    // title: title,
-    // name: name,
+    title: "About",
   });
 });
 
-// about 라우팅
-app.get("/about", function (req, res) {
-  res.render("pages/about.ejs", {
-    title: "About",
-    name: "홍길동",
-    age: 30,
-    address: "안양",
-  });
+// 요청
+app.get("/create", (req, res) => {
+  const title = "어바웃";
+  //form (클라이언트)에서 요청한 name 값과 변수 title을 어바웃페이지에게 랜더 한다. 이때 반드시 폼 화면이 새로고침되면서 랜더되는 페이지로 이동된다.
+  let name = req.query.name;
+  console.log(name);
+  res.render("pages/about.ejs", { title, name });
+});
+
+// 커피 제품 페이지 (coffee 주소로 받은 요청 정보를 커피페이지에 랜더한다.)
+app.get("/coffee", function (req, res) {
+  const title = "커피";
+  let name = req.query.name;
+  console.log(name);
+  res.render("pages/coffee.ejs", { title, name });
 });
 
 // 서버 주소
