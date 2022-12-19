a4b05d7753bb88db2c57ea4e854bf31c9da1bb48
geniunahn <geniunahn@naver.com>
2022-12-19 12:02:09 +0900

    ejs 경로지정까지함

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const app = express();
 // ejs를 view 엔진으로 설정
 app.set("view engine", "ejs");
 
-// ejs 연결
+// 정적파일 경로 지정
+app.use(express.static("public"));
+
+// ejs 문서로 연결하는 경로, 라우팅
 app.get("/", (req, res) => {
   res.render("pages/index.ejs");
+});
+
+// about 라우팅
+app.get("/about", function (req, res) {
+  res.render("pages/about.ejs");
 });
 
 // 서버 주소
